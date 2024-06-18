989a2dede6df7abc3629f3bf479e3b146fb83888
Wang Wenlin <sopl.wang@gmail.com>
2024-06-18 21:21:26 +0800

    chore: Update code files with missing implementations and fix test assertions

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -31,12 +30,23 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = vec![];
+
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let mut new_string = string.clone();
+            match command {
+                Command::Uppercase => {
+                    new_string = new_string.to_uppercase();
+                }
+                Command::Trim => {
+                    new_string = new_string.trim().to_string();
+                }
+                Command::Append(n) => {
+                    new_string = new_string + &"bar".repeat(*n);
+                }
+            }
+            output.push(new_string);
         }
         output
     }
@@ -44,8 +54,7 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
